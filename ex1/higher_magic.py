3e5be592c8f2bcfb3d585d14c5654b3b20c0b48a
Sergio Alejandro <sergioromero2k@gmail.com>
2026-02-27 10:01:09 +0100

    updating readme

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
 
+from collections.abc import Callable
+
+
+def spell_combiner(spell1: Callable, spell2: Callable) -> callable:
+    def combined(*args, **kwargs):
+        r1 = spell1(*args, **kwargs)
+        r2 = spell2(*args, **kwargs)
+        return (r1, r2)
+
+    return combined
+
+
+def power_amplifier(base_spell: callable, multiplier: int) -> callable: ...
+
+
+def conditional_caster(condition: callable, spell: callable) -> callable: ...
+
+
+def spell_sequence(spells: list[callable]) -> callable: ...
+
+
+def main() -> None:
+    fireball = lambda x: f"Fire {x}"
+    heal = lambda x: f"heal {x}"
+
+    combo = spell_combiner(fireball, heal)
+    print(combo("Dragon"))
+
+
+if __name__ == "__main__":
+    main()
